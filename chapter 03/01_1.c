8e3cf23b6ac8a206510b531d732333d6bbd1722a
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-11-07 21:47:56 +0100

    Create 01_1.c

---
+++ chapter 03/01_1.c
@@ -0,0 +1,5 @@
+/*
+Problema:
+Siano dati i tre coefficienti a, b e c di un'equazione di II grado del tipo ax^2+bx+c = 0.
+Si scriva un programma C che legga i 3 coefficienti e determini le soluzioni dell'equazione.
+*/
