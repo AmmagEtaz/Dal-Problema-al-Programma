064cdaca46d0bbe5b2c132799919a9a05dc79c63
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-11-10 19:47:42 +0100

    Update Crittografia semplice di un file di testo.c

--- 3.2 Problemi di codifica- decodifica- transcodifica/3.2.2. Problemi su valori non numerici-ovvero caratteri/Crittografia semplice di un file di testo.c
+++ 3.2 Problemi di codifica- decodifica- transcodifica/3.2.2. Problemi su valori non numerici-ovvero caratteri/Crittografia semplice di un file di testo.c
@@ -1,1 +1,14 @@
-
+/****************************************************************************
+Sia dato un file di testo, il cui nome (stringa di al massimo 30 caratteri,
+terminatore incluso) è letto da tastiera. Si scriva un programma C che,
+letto il file, modifichi la codifica di ciascuno dei suoi caratteri secondo
+le seguenti regole:
+- la codifica n di ogni cifra decimale 0...9 viene trasformata nella
+codifica complemento-a-9 (9-n)
+- ogni lettera minuscola a ... z viene trasformata nella corrispondente 
+maiuscola e la codifica ch così ottenuta viene trasformata nella codifica
+complemento-a-'Z'
+-ogni lettera maiuscola A ... Z viene trasformata nella corrispondente 
+minuscola e la codifica ch così ottenuta viene trasformata nella codifica
+complement-a-'z'
+****************************************************************************/
