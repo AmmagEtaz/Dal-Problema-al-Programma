bcd6896108048fd159a7dc5642828099a2bec6b8
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-11-12 01:49:15 +0100

    Create Menu con scelta su singolo carattere.c

---
+++ 3.3 Problemi di elaborazione testi- text-processing/3.3.1 Confronto-selezione di caratteri-stringhe/Menu con scelta su singolo carattere.c
@@ -0,0 +1,16 @@
+/********************************************************************************
+Si scriva una funzione C che, ripetutamente, acquisisca da tastiera una stringa
+(al massimo di 50 caratteri, contenente eventuali spazi). In questa stirnga il
+primo carattere diverso da spazio costituisce il selettore. I casi da prevedere
+sono i seguenti:
+
+- il carattere selettore è 'U' o 'u'(uscita): uscire dal menu, terminando
+  l'iterazione.
+- il carattere selettore è 'A' o 'a': chiamare la funzione fA, passando come 
+  parametro il resto della stringa
+- il carattere selettore è 'L' o 'l': chiamare la funzione fL, passando come
+  parametro il resto della stringa
+- il carattere selettore è 'T' o 't': chiamare la funzione fT, passando come
+  parametro il resto della stringa
+- ogni altro carattere selettore va segnalato come errato
+*********************************************************************************/
